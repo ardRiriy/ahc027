83b59937ff0205133a82a0bf9d67a070dbfa09b1
ardririy <hynnkgw27@gmail.com>
2023-12-01 23:36:31 +0900

    移動可能かどうかを簡単に判定できるようにした (#4)
    * 壁をすり抜けられるかの判定を構造体に分けた
    
    * setting files update
    
    * add sample input

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -9,12 +9,33 @@
 */
 
 
+use std::sync::Mutex;
+use lazy_static::lazy::Lazy;
 // -*- coding:utf-8-unix -*-
 use proconio::input;
 use proconio::marker::Chars;
 
+struct Walls {
+    wh: Vec<Vec<char>>,
+    ww: Vec<Vec<char>>,
+}
 
-fn dfs(i: usize, j: usize, n: usize ,visited: &mut Vec<Vec<bool>>, wall_h: &Vec<Vec<char>>, wall_w: &Vec<Vec<char>>) {
+impl Walls {
+    pub fn is_through(&self, i: usize, j: usize, ni: isize, nj: isize, n: usize, r: usize) -> bool {
+        if ni < 0 || nj < 0 || ni >= n as isize || nj >= n as isize {
+            return false;
+        }
+        let dj: Vec<isize> = vec![-1, 0, 1, 0];
+        let di: Vec<isize> = vec![0, 1, 0, -1];
+        return if (di[r] == 0 && self.ww[i][j.min(nj as usize)] == '0') || (dj[r] == 0 && self.wh[i.min(ni as usize)][j] == '0') {
+            true
+        } else {
+            false
+        }
+    }
+}
+
+fn dfs(i: usize, j: usize, n: usize ,visited: &mut Vec<Vec<bool>>, walls: &Walls) {
     let dj: Vec<isize> = vec![-1, 0, 1, 0];
     let di: Vec<isize> = vec![0, 1, 0, -1];
     let r#move = vec!['L', 'D', 'R', 'U'];
@@ -22,12 +43,10 @@ fn dfs(i: usize, j: usize, n: usize ,visited: &mut Vec<Vec<bool>>, wall_h: &Vec<
     for r in 0..4 {
         let ni = i as isize + di[r];
         let nj = j as isize + dj[r];
-        if ni < 0 || nj < 0 || ni >= n as isize || nj >= n as isize || visited[ni as usize][nj as usize] {
-            continue;
-        }
-        if (di[r] == 0 && wall_w[i][j.min(nj as usize)] == '0') || (dj[r] == 0 && wall_h[i.min(ni as usize)][j] == '0') {
+
+        if Walls::is_through(&walls, i, j, ni, nj, n, r) && !visited[ni as usize][nj as usize]{
             print!("{}", r#move[r]);
-            dfs(ni as usize, nj as usize, n ,visited, wall_h, wall_w);
+            dfs(ni as usize, nj as usize, n ,visited, walls);
             print!("{}", r#move[(r + 2) % 4]);
         }
     }
@@ -41,7 +60,10 @@ fn solve(){
         d: [[usize; n]; n],
     }
 
-    dfs(0, 0, n, &mut vec![vec![false; n]; n], &wall_h, &wall_v);
+    // init
+    let walls = Walls{wh: wall_h, ww: wall_v};
+
+    dfs(0, 0, n, &mut vec![vec![false; n]; n], &walls);
     println!();
 
 }
