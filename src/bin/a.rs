b406f556ea27c703e883d1ac118af5d71c973f63
ardririy <hynnkgw27@gmail.com>
2023-12-06 01:05:31 +0900

    高速化 (#16)
    * 各頂点からの距離を前計算
    
    * 高速化した。最初の2倍ちょっとまで早くなった。
    
    * アロケート周りを見直して、ちょっとだけ高速化。50k弱イテレーションまで回せた。
    
    * なんか悔しかったのでseed=97で50kイテレーションまで載せた

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -20,6 +20,11 @@ use rand::Rng;
 
 static INF: usize = 1e18 as usize;
 static AREAS: usize = 16;
+static DI:[isize; 4] = [0, 1, 0, -1];
+static DJ:[isize; 4] = [-1, 0, 1, 0];
+static MOVE: [char; 4] = ['L', 'D', 'R', 'U'];
+static TL: f64 = 1.97;
+
 struct Walls {
     wh: Vec<Vec<char>>,
     ww: Vec<Vec<char>>,
@@ -27,16 +32,13 @@ struct Walls {
 
 impl Walls {
     pub fn is_through(&self, i: usize, j: usize, n: usize, r: usize) -> bool {
-        let di: Vec<isize> = vec![0, 1, 0, -1];
-        let dj: Vec<isize> = vec![-1, 0, 1, 0];
-
-        let ni = i as isize + di[r];
-        let nj = j as isize + dj[r];
+        let ni = i as isize + DI[r];
+        let nj = j as isize + DJ[r];
         if ni < 0 || nj < 0 || ni >= n as isize || nj >= n as isize {
             return false;
         }
 
-        return if (di[r] == 0 && self.ww[i][j.min(nj as usize)] == '0') || (dj[r] == 0 && self.wh[i.min(ni as usize)][j] == '0') {
+        return if (DI[r] == 0 && self.ww[i][j.min(nj as usize)] == '0') || (DJ[r] == 0 && self.wh[i.min(ni as usize)][j] == '0') {
             true
         } else {
             false
@@ -103,23 +105,11 @@ static mut STIME: f64 = -1.0;
     }
 }
 
-fn debug_grid(v: &Vec<Vec<usize>>) {
-    for i in 0..v.len() {
-        for j in 0..v[i].len() {
-            print!("{:2} ", v[i][j]);
-        }
-        println!();
-    }
-}
-
 /*任意の地点から(0, 0)に戻る*/
 fn back_to_start(i: usize, j: usize, n: usize, routes: &mut Vec<(usize, usize)> ,walls:&Walls) {
     //{i, j}を始点にBFS
     let mut dist = vec![vec![INF; n]; n];
     let mut before = vec![vec![INF; n]; n];
-
-    let di: Vec<isize> = vec![0, 1, 0, -1];
-    let dj: Vec<isize> = vec![-1, 0, 1, 0];
 
     let mut que = VecDeque::new();
     que.push_back((i, j));
@@ -127,8 +117,8 @@ fn back_to_start(i: usize, j: usize, n: usize, routes: &mut Vec<(usize, usize)> 
     before[i][j] = 4;
     while let Some((pi, pj)) = que.pop_front() {
         for r in 0..4 {
-            let ni = pi as isize + di[r];
-            let nj = pj as isize + dj[r];
+            let ni = pi as isize + DI[r];
+            let nj = pj as isize + DJ[r];
             if Walls::is_through(walls, pi, pj, n, r) && dist[ni as usize][nj as usize] == INF {
                 dist[ni as usize][nj as usize] = dist[pi][pj] + 1;
                 before[ni as usize][nj as usize] = r;
@@ -143,8 +133,8 @@ fn back_to_start(i: usize, j: usize, n: usize, routes: &mut Vec<(usize, usize)> 
 
     while !(now_i == i && now_j == j) {
         stk.push((now_i, now_j));
-        let ni = (now_i as isize + di[(before[now_i][now_j] + 2) % 4]) as usize;
-        let nj = (now_j as isize + dj[(before[now_i][now_j] + 2) % 4]) as usize;
+        let ni = (now_i as isize + DI[(before[now_i][now_j] + 2) % 4]) as usize;
+        let nj = (now_j as isize + DJ[(before[now_i][now_j] + 2) % 4]) as usize;
 
         now_i = ni;
         now_j = nj;
@@ -160,10 +150,7 @@ fn back_to_start(i: usize, j: usize, n: usize, routes: &mut Vec<(usize, usize)> 
 * エリア内をDFSする
 * 非再起で書いて、掃除終了時点の座標を返却
 */
-fn cleanup_area(i: usize, j: usize, n: usize, color: &Vec<Vec<usize>>, routes: &mut Vec<(usize, usize)>, dirts: &mut Vec<Vec<usize>>, d: &Vec<Vec<usize>> ,walls: &Walls) -> (usize, usize) {
-    let di: Vec<isize> = vec![0, 1, 0, -1];
-    let dj: Vec<isize> = vec![-1, 0, 1, 0];
-
+fn cleanup_area(i: usize, j: usize, n: usize, color: &Vec<Vec<usize>>, routes: &mut Vec<(usize, usize)>, walls: &Walls) -> (usize, usize) {
     let mut pos = (i, j); // 今の位置
 
     /* DFS */
@@ -172,8 +159,8 @@ fn cleanup_area(i: usize, j: usize, n: usize, color: &Vec<Vec<usize>>, routes: &
 
     let mut stk = Vec::new();
     for r in 0..4usize {
-        let ni = i as isize+ di[r];
-        let nj = j as isize + dj[r];
+        let ni = i as isize+ DI[r];
+        let nj = j as isize + DJ[r];
         if Walls::is_through(walls, i, j, n, r) && color[ni as usize][nj as usize] == color[i][j] {
             stk.push((ni, nj, r));
         }
@@ -188,17 +175,15 @@ fn cleanup_area(i: usize, j: usize, n: usize, color: &Vec<Vec<usize>>, routes: &
                 visited[p_i as usize][p_j as usize] = true;
                 pos = (p_i as usize, p_j as usize);
                 routes.push((p_i as usize, p_j as usize));
-                update_dirt(dirts, d, &mut vec![0usize; color.len()], color, (p_i as usize, p_j as usize));
-
 
                 // 帰りがけの処理を追加する
                 // 帰りがけはbit反転
-                stk.push((!(p_i + di[(dir + 2) % 4]), p_j + dj[(dir + 2) % 4], (dir + 2) % 4));
+                stk.push((!(p_i + DI[(dir + 2) % 4]), p_j + DJ[(dir + 2) % 4], (dir + 2) % 4));
 
                 // 行きがけの処理を追加する
                 for r in 0..4 {
-                    let ni = p_i + di[r];
-                    let nj = p_j + dj[r];
+                    let ni = p_i + DI[r];
+                    let nj = p_j + DJ[r];
 
                     if Walls::is_through(walls, p_i as usize, p_j as usize, n, r) && color[ni as usize][nj as usize] == color[i][j] {
                         stk.push((ni, nj, r));
@@ -219,7 +204,6 @@ fn cleanup_area(i: usize, j: usize, n: usize, color: &Vec<Vec<usize>>, routes: &
             // 帰りがけは絶対出力
             pos = (!p_i as usize, p_j as usize);
             routes.push((!p_i as usize, p_j as usize));
-            update_dirt(dirts, d, &mut vec![0usize; color.len()], color, (!p_i as usize, p_j as usize));
         }
     }
     pos
@@ -236,31 +220,28 @@ fn solve(){
     // init
     let walls = Walls{wh: wall_h, ww: wall_v};
 
-    let di: Vec<isize> = vec![0, 1, 0, -1];
-    let dj: Vec<isize> = vec![-1, 0, 1, 0];
-    let r#move = vec!['L', 'D', 'R', 'U'];
-
-
     let mut rng = rand::thread_rng();
 
     // エリア分け
     let mut color = vec![vec![INF; n]; n];
-    let mut que = VecDeque::new();
-    // TODO: AREASを使って書き換えたい
-    for i in 0..4 {
-        for j in 0..4 {
-            color[(i * n / 4) + 2][(j * n / 4) + 2] = i * 4 + j;
-            que.push_back(((i * n / 4) + 2, (j * n / 4) + 2));
+    {
+        let mut que = VecDeque::new();
+        // TODO: AREASを使って書き換えたい
+        for i in 0..4 {
+            for j in 0..4 {
+                color[(i * n / 4) + 2][(j * n / 4) + 2] = i * 4 + j;
+                que.push_back(((i * n / 4) + 2, (j * n / 4) + 2));
+            }
         }
-    }
 
-    while let Some((p_i, p_j)) = que.pop_front() {
-        for r in 0..4 {
-            let ni = p_i as isize + di[r];
-            let nj = p_j as isize + dj[r];
-            if Walls::is_through(&walls, p_i, p_j, n, r) && color[ni as usize][nj as usize] == INF {
-                color[ni as usize][nj as usize] = color[p_i][p_j];
-                que.push_back((ni as usize, nj as usize));
+        while let Some((p_i, p_j)) = que.pop_front() {
+            for r in 0..4 {
+                let ni = p_i as isize + DI[r];
+                let nj = p_j as isize + DJ[r];
+                if Walls::is_through(&walls, p_i, p_j, n, r) && color[ni as usize][nj as usize] == INF {
+                    color[ni as usize][nj as usize] = color[p_i][p_j];
+                    que.push_back((ni as usize, nj as usize));
+                }
             }
         }
     }
@@ -281,11 +262,35 @@ fn solve(){
         }
         while let Some((p_i, p_j)) = que.pop_front() {
             for r in 0..4 {
-                let ni = p_i as isize + di[r];
-                let nj = p_j as isize + dj[r];
+                let ni = p_i as isize + DI[r];
+                let nj = p_j as isize + DJ[r];
                 if Walls::is_through(&walls, p_i, p_j, n, r) && dist_from_area[clr][ni as usize][nj as usize] == INF {
                     dist_from_area[clr][ni as usize][nj as usize] = dist_from_area[clr][p_i][p_j] + 1;
                     que.push_back((ni as usize, nj as usize));
+                }
+            }
+        }
+    }
+
+    /*
+    各頂点からの距離をそれぞれ調べる
+    */
+    let mut dist_from_point = vec![vec![vec![INF; n]; n]; n * n];
+    for i in 0..n {
+        for j in 0..n {
+            let idx = i * n + j;
+
+            dist_from_point[idx][i][j] = 0;
+            let mut que = VecDeque::new();
+            que.push_back((i, j));
+            while let Some((p_i, p_j)) = que.pop_front() {
+                for r in 0..4 {
+                    let ni = p_i as isize + DI[r];
+                    let nj = p_j as isize + DJ[r];
+                    if Walls::is_through(&walls, p_i, p_j, n, r) && dist_from_point[idx][ni as usize][nj as usize] == INF {
+                        dist_from_point[idx][ni as usize][nj as usize] = dist_from_point[idx][p_i][p_j] + 1;
+                        que.push_back((ni as usize, nj as usize));
+                    }
                 }
             }
         }
@@ -297,10 +302,12 @@ fn solve(){
        => 平均で
     */
     let mut cnt = vec![0usize; AREAS];
+    let mut sum_d = vec![0usize; AREAS];
 
     for i in 0..n {
         for j in 0..n {
             cnt[color[i][j]] += 1;
+            sum_d[color[i][j]] += d[i][j];
         }
     }
 
@@ -309,43 +316,66 @@ fn solve(){
       はじめ、色0が全部0で、汚れやすさが一番でかいところを掃除しに行く
     */
     let mut dirt = vec![0usize; AREAS];
+    let mut permutation = vec![color[0][0]];
     let mut cleaned = vec![false; AREAS]; // 一度でも掃除済みか否かを判定
-    let mut room_dirt = vec![vec![0usize; n]; n];
     let mut tracking_route = vec![(0usize, 0usize)]; // 到達順番を追跡
+    cleaned[color[0][0]] = true;
 
-    let mut now_pos = (0usize, 0usize);
+    for i in 0..16 {
+        if color[0][0] != i {
+            dirt[i] = sum_d[i].sqrt();
+        }
+    }
+
     while !cleaned.iter().all(|b| *b) {
-        update_dirt(&mut room_dirt, &d, &mut dirt, &color, now_pos);
-
         // 一番汚れているエリアを探す
         let mut max_idx = 0usize;
         let mut max_dirt = 0usize;
         for i in 0..AREAS {
-            if max_dirt < dirt[i] / cnt[i] {
-                max_dirt = dirt[i] / cnt[i];
+            if max_dirt < dirt[i] {
+                max_dirt = dirt[i];
                 max_idx = i;
             }
         }
 
-        // 次のエリアに移動
-        while dist_from_area[max_idx][now_pos.0][now_pos.1] != 0 {
-            // 距離が-1になる場所に移動
-            for r in 0..4 {
-                let ni = now_pos.0 as isize + di[r];
-                let nj = now_pos.1 as isize + dj[r];
-                if Walls::is_through(&walls, now_pos.0, now_pos.1, n, r)
-                    && dist_from_area[max_idx][ni as usize][nj as usize] + 1 == dist_from_area[max_idx][now_pos.0][now_pos.1] {
-                    now_pos.0 = ni as usize;
-                    now_pos.1 = nj as usize;
-                    tracking_route.push((now_pos.0, now_pos.1));
-                    update_dirt(&mut room_dirt, &d, &mut dirt, &color, now_pos);
-                    break;
+        cleaned[max_idx] = true;
+        permutation.push(max_idx);
+
+        // 汚れを更新
+        for i in 0..AREAS {
+            if i == max_idx {
+                dirt[i] = 0;
+            }else{
+                dirt[i] += sum_d[i].sqrt();
+            }
+        }
+    }
+
+    let mut now_pos = (0usize, 0usize);
+    for (idx, &next) in permutation.iter().enumerate() {
+        let (mut p_i, mut p_j) = now_pos;
+        // まずは掃除をしてもらう
+        (p_i, p_j) = cleanup_area(p_i, p_j, n, &color, &mut tracking_route, &walls);
+
+        if idx != permutation.len() - 1 {
+            // 最後でなければ次のエリアに移動
+            let next_area = permutation[idx + 1];
+            while dist_from_area[next_area][p_i][p_j] != 0 {
+                // 距離が-1になる場所に移動
+                for r in 0..4 {
+                    let ni = p_i as isize + DI[r];
+                    let nj = p_j as isize + DJ[r];
+                    if Walls::is_through(&walls, p_i, p_j, n, r)
+                        && dist_from_area[next_area][ni as usize][nj as usize] + 1 == dist_from_area[next_area][p_i][p_j] {
+                        p_i = ni as usize;
+                        p_j = nj as usize;
+                        tracking_route.push((p_i, p_j));
+                        break;
+                    }
                 }
             }
         }
-        // max_idxを掃除
-        now_pos = cleanup_area(now_pos.0, now_pos.1, n, &color, &mut tracking_route, &mut room_dirt, &d ,&walls);
-        cleaned[max_idx] = true;
+        now_pos = (p_i, p_j);
     }
 
     back_to_start(now_pos.0, now_pos.1, n, &mut tracking_route ,&walls);
@@ -360,8 +390,11 @@ fn solve(){
         passing_times[tracking_route[i].0][tracking_route[i].1] += 1;
     }
 
-    let mut cnt = 1usize;
-    'annealing: while get_time() < 1.9 {
+    let start_temp = 50isize;
+    let end_temp = 10isize;
+    let mut cnt = 0usize;
+    let mut new_route;
+    'annealing: while get_time() < TL {
         // memo: idxとidx + rangeは、"接続先"であって、ここは変えない
         let idx = rng.gen_range(0..tracking_route.len());
         let range = rng.gen_range(5..100); // 現状の何手先まで変えるか 値は適当. 最後にidx+rangeに接続できないと行けない
@@ -379,42 +412,22 @@ fn solve(){
             }
         }
 
-        cnt += 1;
-
-
         // 新しいルートは、とりあえずBFS。基本的には短いほうがいいので。
         // 実際に改善するかどうかは、あとで分かるのでなんでもいいのです。
         // ↑本当はどうでも良くはないんだけど、まぁまだあと5日あるので多少は...
 
-        let mut que = VecDeque::new();
-        que.push_back((tracking_route[idx].0, tracking_route[idx].1));
-        let mut field = vec![vec![INF; n]; n];
-        field[tracking_route[idx].0][tracking_route[idx].1] = 0;
-        'bfs: while let Some((p_i, p_j)) = que.pop_front() {
-            for r in 0..4 {
-                let ni = p_i as isize + di[r];
-                let nj = p_j as isize + dj[r];
-                if walls.is_through(p_i, p_j, n, r) && field[ni as usize][nj as usize] == INF {
-                    field[ni as usize][nj as usize] = field[p_i][p_j] + 1;
-                    que.push_back((ni as usize, nj as usize));
-                    if (ni as usize, nj as usize) == tracking_route[idx + range] {
-                        break 'bfs;
-                    }
-                }
-            }
-        }
-
         let mut pos = tracking_route[idx + range];
-        let mut new_route = tracking_route.clone();
+        new_route = tracking_route.clone();
+        cnt += 1;
 
         let mut update = vec![];
-        while field[pos.0][pos.1] != 0 {
+        while dist_from_point[tracking_route[idx].0 * n + tracking_route[idx].1][pos.0][pos.1] != 0 {
             // 距離が-1になる場所に移動
             for r in 0..4 {
-                let ni = pos.0 as isize + di[r];
-                let nj =pos.1 as isize + dj[r];
+                let ni = pos.0 as isize + DI[r];
+                let nj =pos.1 as isize + DJ[r];
                 if walls.is_through(pos.0, pos.1, n, r)
-                    && field[ni as usize][nj as usize] + 1 == field[pos.0][pos.1] {
+                    && dist_from_point[tracking_route[idx].0 * n + tracking_route[idx].1][ni as usize][nj as usize] + 1 == dist_from_point[tracking_route[idx].0 * n + tracking_route[idx].1][pos.0][pos.1] {
                     pos.0 = ni as usize;
                     pos.1 = nj as usize;
                     update.push(pos);
@@ -427,8 +440,10 @@ fn solve(){
         new_route.splice(idx+1..idx+range, update.clone());
         let new_score = evaluate(n, &d, &new_route);
 
+        let temp = start_temp as f64 + (end_temp - start_temp) as f64 * get_time() / TL;
+        let prob = ((prev_score - new_score) as f64 / temp).exp();
 
-        if prev_score > new_score {
+        if prob > (rng.gen_range(0..INF) % INF) as f64 / INF as f64 {
             // 改善しているのなら採用
             prev_score = new_score;
 
@@ -441,7 +456,6 @@ fn solve(){
 
             swap(&mut tracking_route, &mut new_route);
         }
-
     }
 
     // tracking_routeを元に答えを出力
@@ -449,40 +463,24 @@ fn solve(){
         let dy = tracking_route[i+1].0 as isize - tracking_route[i].0 as isize;
         let dx = tracking_route[i+1].1 as isize - tracking_route[i].1 as isize;
         for r in 0..4 {
-            if di[r] == dy && dj[r] == dx {
-                print!("{}", r#move[r]);
+            if DI[r] == dy && DJ[r] == dx {
+                print!("{}", MOVE[r]);
                 break;
             }
         }
     }
     println!();
+    // println!("{}", cnt);
 }
-
-fn update_dirt(dirts: &mut Vec<Vec<usize>>, d: &Vec<Vec<usize>>, area_dirt: &mut Vec<usize>, color: &Vec<Vec<usize>>, p: (usize, usize)) {
-    area_dirt.fill(0);
-
-    // 汚れの更新
-    for i in 0..dirts.len() {
-        for j in 0..dirts[i].len() {
-            dirts[i][j] += d[i][j].sqrt();
-            area_dirt[color[i][j]] += dirts[i][j];
-        }
-    }
-
-    // 今いる地点は掃除されるので値は0に
-    area_dirt[color[p.0][p.1]] -= dirts[p.0][p.1];
-    dirts[p.0][p.1] = 0;
-}
-
 
 fn main() {
     let mut i: usize = 1;
     get_time();
-/*    /* 複数テストケースならコメントアウトを外す */
-    let mut input = String::new();
-    io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut input).unwrap();
-    i = input.trim().parse().unwrap();*/
+    /*    /* 複数テストケースならコメントアウトを外す */
+        let mut input = String::new();
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut input).unwrap();
+        i = input.trim().parse().unwrap();*/
 
     while i != 0 {
         solve();
